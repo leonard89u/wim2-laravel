9c7fc33f4d63b363131c6ef8580a7c00a5c87159
leonard89u <leonard89u@univ-lorraine.fr>
2019-09-30 10:30:15 +0200

    Exercice 3

---
+++ resources/views/a-propos.blade.php
@@ -0,0 +1,2 @@
+<h1>A propos</h1> 
+<p>Cette application a été créée dans le cadre du cours de programmation côté serveur.</p>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<h1>Contactez-nous</h1> 
+<em>Ma super entreprise</em> 
+<p>03 .. .. .. ..</p> 
